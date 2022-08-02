dee735855f889209b154e934056894fa8c53cfd3
Baumll <adriangame@gmx.de>
2022-08-02 10:50:55 +0200

    ROS conventions

--- scripts/musclework.py
+++ scripts/musclework.py
@@ -12,9 +12,59 @@ import time
 from scipy import signal
 import os
 
-class ChangePointFinder():
+class Musclework():
     def __init__(self):
-        self.listener()
+        #Paramter
+        self.maxTimeSec = 1
+        self.lastElements = 100
+        self.past = 5
+        self.bs = []
+        self.timeStamps = []
+        self.score = []
+        self.angles = []
+        self.changePoints = []
+        self.angleNames = [
+            "upper_arm_left",
+            "upper_arm_right",
+            "lower_arm_left",
+            "lower_arm_right",
+            "neck",
+            "trunk",
+            "legs",
+            "wrist_left",
+            "wrist_right"]
+
+            
+        #Pub sub
+        rospy.Subscriber("/ergonomics/joint_angles", Float32MultiArray, self.callback)
+        self.pub = rospy.Publisher('/musclework', Int8, queue_size=10)
+
+        #Speichert die daten:
+        """     df = pd.DataFrame()
+        df["upper_arm_left"] = self.angles[0]
+        df["upper_arm_left_cp"] = self.bs[0].probabilities[-1][1:]
+        df["upper_arm_right"] = self.angles[1]
+        df["upper_arm_right_cp"] = self.bs[1].probabilities[-1][1:]
+        df["lower_arm_left"] = self.angles[2]
+        df["lower_arm_left_cp"] = self.bs[2].probabilities[-1][1:]
+        df["lower_arm_right"] = self.angles[3]
+        df["lower_arm_right_cp"] = self.bs[3].probabilities[-1][1:]
+        df["neck"] = self.angles[4]
+        df["neck_cp"] = self.bs[4].probabilities[-1][1:]
+        df["trunk"] = self.angles[5]
+        df["trunk_cp"] = self.bs[5].probabilities[-1][1:]
+        df["legs"] = self.angles[6]
+        df["legs_cp"] = self.bs[6].probabilities[-1][1:]
+        df["wrist_left"] = self.angles[7]
+        df["wrist_left_cp"] = self.bs[7].probabilities[-1][1:]
+        df["wrist_right"] = self.angles[8]
+        df["wrist_right_cp"] = self.bs[8].probabilities[-1][1:]
+
+        os.remove("/home/adrian/Documents/Uni/BachelorArbeit/ba_workspace/src/ba_package/csv_data/outCPD.csv")
+        df.index.name = 'row'
+        df.to_csv("/home/adrian/Documents/Uni/BachelorArbeit/ba_workspace/src/ba_package/csv_data/outCPD.csv")
+        print("save") """
+        
         
 
     def callback(self,data):
@@ -55,71 +105,14 @@ class ChangePointFinder():
                     if time.time_ns() - self.timeStamps[self.changePoints[i][-1]] < 1e+9*self.maxTimeSec:
                         plusScore = 1
 
+        #publish extra points
         #print("Time Needed: " + str((time.time_ns() - tmp_time)/1e+9) + " sec.")
         self.pub.publish(plusScore)
         if(plusScore > 0):
-            print("Score plus: " + str(plusScore))
-
-        
+            print("Score: " + str(plusScore))
 
 
-    def listener(self):
 
-        print("Setup listener")
-        rospy.init_node('listener', anonymous=True)
-
-
-        #Paramter
-        self.maxTimeSec = 1
-        self.lastElements = 100
-        self.past = 5
-        self.bs = []
-        self.timeStamps = []
-        self.score = []
-        self.angles = []
-        self.changePoints = []
-        self.angleNames = [
-            "upper_arm_left",
-            "upper_arm_right",
-            "lower_arm_left",
-            "lower_arm_right",
-            "neck",
-            "trunk",
-            "legs",
-            "wrist_left",
-            "wrist_right"]
-        rospy.Subscriber("/ergonomics/joint_angles", Float32MultiArray, self.callback)
-        self.pub = rospy.Publisher('/plusScore', Int8, queue_size=10)
-
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
-        
-
-        #Speichert die daten:
-"""         df = pd.DataFrame()
-        df["upper_arm_left"] = self.angles[0]
-        df["upper_arm_left_cp"] = self.bs[0].probabilities[-1][1:]
-        df["upper_arm_right"] = self.angles[1]
-        df["upper_arm_right_cp"] = self.bs[1].probabilities[-1][1:]
-        df["lower_arm_left"] = self.angles[2]
-        df["lower_arm_left_cp"] = self.bs[2].probabilities[-1][1:]
-        df["lower_arm_right"] = self.angles[3]
-        df["lower_arm_right_cp"] = self.bs[3].probabilities[-1][1:]
-        df["neck"] = self.angles[4]
-        df["neck_cp"] = self.bs[4].probabilities[-1][1:]
-        df["trunk"] = self.angles[5]
-        df["trunk_cp"] = self.bs[5].probabilities[-1][1:]
-        df["legs"] = self.angles[6]
-        df["legs_cp"] = self.bs[6].probabilities[-1][1:]
-        df["wrist_left"] = self.angles[7]
-        df["wrist_left_cp"] = self.bs[7].probabilities[-1][1:]
-        df["wrist_right"] = self.angles[8]
-        df["wrist_right_cp"] = self.bs[8].probabilities[-1][1:]
-
-        os.remove("/home/adrian/Documents/Uni/BachelorArbeit/ba_workspace/src/ba_package/csv_data/outCPD.csv")
-        df.index.name = 'row'
-        df.to_csv("/home/adrian/Documents/Uni/BachelorArbeit/ba_workspace/src/ba_package/csv_data/outCPD.csv")
-        print("save") """
 
 
 def plotter():
@@ -137,12 +130,18 @@ def createInt8MultiArray(self,data):
     dim.size = len(data)
     msg.layout.dim.append(dim)
     msg.data = data
-    return msg     
+    return msg
+
+def musclework_start():
+    print("Setup Musclework")
+    rospy.init_node('Musclework', anonymous=True)
+    process = Musclework()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
-        
-        ChangePointFinder()
+        musclework_start()
     except rospy.ROSInterruptException:
         pass
         
